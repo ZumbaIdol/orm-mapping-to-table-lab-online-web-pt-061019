95de1fdc60f6a56ff078de88d4dcced2bef06bf8
David Foote <david.a.foote@gmail.com>
2019-08-19 18:47:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,6 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (?, ?)
       SQL
-      DB[:conn].execute(self.name, self.grade)
+      DB[:conn].execute(sql, self.name, self.grade)
   end
 end
